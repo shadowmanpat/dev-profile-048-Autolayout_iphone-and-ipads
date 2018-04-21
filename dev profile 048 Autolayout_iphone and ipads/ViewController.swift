e51b0ac9d246310244462cc9064658e76ac78a1f
Nikolaos Agas <agascompany@gmail.com>
2018-04-21 15:53:25 +0300

    Initial Commit

---
+++ dev profile 048 Autolayout_iphone and ipads/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dev profile 048 Autolayout_iphone and ipads
+//
+//  Created by Nikolaos Agas on 21/04/2018.
+//  Copyright © 2018 Nikolaos Agas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
